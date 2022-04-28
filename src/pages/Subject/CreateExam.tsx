9b7453a5ea6dc20947797fc35656adb6a26f4d5d
nhattruongagtm <18130261@st.hcmuaf.edu.vn>
2022-04-28 15:51:38 +0700

    creact test ui

--- src/pages/Subject/CreateExam.tsx
+++ src/pages/Subject/CreateExam.tsx
@@ -1,6 +1,8 @@
-import { Button, Form, Input } from 'antd'
+import { Button, DatePicker, Form, Input, Select } from 'antd'
 import React from 'react'
+import Question from '../../components/Content/Test/Question'
 
+const { Option } = Select
 type Props = {}
 
 const CreateExam = (props: Props) => {
@@ -11,77 +13,87 @@ const CreateExam = (props: Props) => {
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo)
   }
+
+  function onChange(value: any) {
+    console.log(`selected ${value}`)
+  }
+
+  function onSearch(val: any) {
+    console.log('search:', val)
+  }
   return (
     <div className="create__exam">
       <div className="create__exam__main">
         <div className="create__exam__title">
-          <h3>Tạo đề thi môn: Nhập môn CNPM</h3>
+          <h3>
+            Tạo đề thi môn: <span>Nhập môn CNPM</span>
+          </h3>
+          <div className="create__exam__form">
+            <div className="create__exam__options">
+              <div className="create__exam__options__item">
+                <h3 className="options__title">Chọn ngày thi</h3>
+                <DatePicker />
+              </div>
+              <div className="create__exam__options__item">
+                <h3 className="options__title">Chọn giờ thi</h3>
+                <Select
+                  placeholder="Select an exam time"
+                  onChange={onChange}
+                  onSearch={onSearch}
+                >
+                  <Option value="jack">7:30</Option>
+                  <Option value="lucy">9:30</Option>
+                  <Option value="tom">12:30</Option>
+                  <Option value="dsf">14:30</Option>
+                </Select>
+              </div>
+              <div className="create__exam__options__item btn__upload">
+                <Button type="primary">Upload File</Button>
+              </div>
+            </div>
+            <Form
+              name="basic"
+              labelCol={{
+                span: 8,
+              }}
+              wrapperCol={{
+                span: 16,
+              }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+              className="create__form"
+            >
+              <div className="form__title">
+                <span>Câu hỏi: 1</span>
+                <Input placeholder="Enter your question" />
+              </div>
+              <div className="form__title">
+                <span>Đáp án:</span>
+                <Input placeholder="Enter your answer 1" />
+                <Input placeholder="Enter your answer 2" />
+                <Input placeholder="Enter your answer 3" />
+                <Input placeholder="Enter your answer 4" />
+              </div>
+            </Form>
+          </div>
         </div>
-        <Form
-          name="basic"
-          labelCol={{
-            span: 8,
-          }}
-          wrapperCol={{
-            span: 16,
-          }}
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your username!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your password!',
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          ></Form.Item>
-
-          <Form.Item
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
       </div>
       <div className="create__exam__list">
         <div className="create__exam__scroll">
-          <div className="create__exam__item"></div>
+          <div className="question__list__item">
+            <div className="question__title">
+              Câu 1: <span>{'Ai đã đặt tên cho dòng sông?'}</span>
+              <Button>
+                <i className="bx bxs-flag"></i>
+              </Button>
+            </div>
+            <ul className="question__answers">
+              <li>
+                <span>hello</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
